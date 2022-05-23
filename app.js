a86c5ebb0506697598c31af57234301aab87b06f
Martia <pureromancebymartia@gmail.com>
2022-05-22 20:58:32 -0400

    welcome

--- app.js
+++ app.js
@@ -1,30 +1,81 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const blogRoutes = require('./routes/blogRoutes');
+
+
 
 // express app
 const app = express();
+
+//connection to mongodb
+const dbURI = 'mongodb+srv://Ichigo:shinigami@nodemodules.v1dki.mongodb.net/nodeModules?retryWrites=true&w=majority';
+mongoose.connect(dbURI, {useNewURLParser: true, useUnifiedTopology: true })
+.then((result) => app.listen(3000))
+.catch((err) => console.log(err));
+
 
 
 //register view engine
 app.set('view engine', 'ejs');
 
 
+
 // listen for requests
 
-app.listen(3000);
 
-//middleware and statc files
+
+
+//middleware and static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
 app.use(morgan ('dev')); 
-    
+
+//mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: 'new blog2',
+    snippet: 'about my new blog',
+    body: 'more about my new blog'
+});
+
+blog.save()
+.then((result) => {
+    res.send(result)
+})
+.catch((err) => {
+    console.log(err);
+});
+})
   
+app.get('/all-blogs', (req, res) => {
+    Blog.find()
+    .then((result) => {
+        res.send(result);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+})
+  
+app.get('/single-blog', (req, res) => {
+    Blog.findById('628aa3fd869c27b3b65f5c10')
+    .then((result) => {
+        res.send(result)
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+})
 
 app.get('/',(req, res) => {
-    const blogs = [
+    res.redirect('/blogs');
+   /* const blogs = [
         {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
         {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
         {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      ];
+      ];*/
     
     
 
@@ -38,10 +89,9 @@ app.get('/about',(req, res) => {
 res.render('about', { title: 'About' });
 });
 
-app.get('/blogs/create',(req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-})
 
+//blog routes
+app.use('/blogs', blogRoutes);
 
 //404
 app.use((req, res) => {
